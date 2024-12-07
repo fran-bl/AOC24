990de1e262bbaceae7e1b8d27556e3b03f68f3d9
fran-bl <fran.blazevic0@gmail.com>
2024-12-07 15:51:24 +0100

    Optimized day 6

--- day_6/Main.py
+++ day_6/Main.py
@@ -1,7 +1,3 @@
-import copy
-from tqdm import tqdm
-
-
 def get_start_pos(mat):
     height, width = len(mat), len(mat[0])
 
@@ -25,8 +21,19 @@ def get_obstacles(mat):
     return obstacles
 
 
+def get_crosses(path):
+    crosses = {}
+
+    for key in path.keys():
+        t = [k for k in path.keys() if k[0] == key[0] and k[1] == key[1]]
+        if len(t) > 1:
+            crosses[key[:2]] = [False, [(k[0] - int(k[2].real), k[1] - int(k[2].imag), k[2], k[3]) for k in t]]
+    
+    return crosses
+
+
 def chk_good_path(obstacles, pos, height, width):
-    exit_dir = -1
+    exit_dir = pos[3]
 
     turns_set = set()
 
@@ -44,6 +51,7 @@ def chk_good_path(obstacles, pos, height, width):
             encoded_pos = f'{pos[0]:03}{pos[1]:03}{entry_dir}{exit_dir}'
 
             if encoded_pos in turns_set:
+
                 return False
             else:
                 turns_set.add(encoded_pos)
@@ -53,27 +61,36 @@ def chk_good_path(obstacles, pos, height, width):
 
 
 def part_1(mat):
-    path = set()
+    path = {}
+    unique = set()
     start = get_start_pos(mat)
     pos = get_start_pos(mat)
     obstacles = get_obstacles(mat)
     height, width = len(mat), len(mat[0])
 
-    exit_dir = -1
+    entry_dir = exit_dir = -1
 
     while 0 <= pos[0] < height and 0 <= pos[1] < width:
-        path.add((pos[0], pos[1]))
+        unique.add((pos[0], pos[1]))
         
         if pos[0] + int(exit_dir.real) < 0 or pos[0] + int(exit_dir.real) >= height or pos[1] + int(exit_dir.imag) < 0 or pos[1] + int(exit_dir.imag) >= width:
+            path[(pos[0], pos[1], entry_dir, exit_dir)] = None
             break
 
         if (pos[0] + int(exit_dir.real), pos[1] + int(exit_dir.imag)) in obstacles:
+            entry_dir = exit_dir
             exit_dir *= -1j
+
+            if 0 <= pos[0] + int(exit_dir.real) < height and 0 <= pos[1] + int(exit_dir.imag) < width and (pos[0] + int(exit_dir.real), pos[1] + int(exit_dir.imag)) in obstacles:
+                exit_dir *= -1j
+
+        path[(pos[0], pos[1], entry_dir, exit_dir)] = None
+        entry_dir = exit_dir
 
         pos[0] += int(exit_dir.real)
         pos[1] += int(exit_dir.imag)
 
-    return len(path), list(path)
+    return len(unique), path
 
 
 def part_2(mat):
@@ -82,18 +99,28 @@ def part_2(mat):
     start = get_start_pos(mat)
     obstacles = get_obstacles(mat)
     _, path = part_1(mat)
+    crosses = get_crosses(path)
     height, width = len(mat), len(mat[0])
 
-    for i in tqdm(range(len(path)), desc='Computing...'):
-        idx = path[i]
-        
-        if idx != (start[0], start[1]):
-            obstacles[idx] = True
-            
-            if not chk_good_path(obstacles, start[:], height, width):
-                count += 1
+    moves = list(path.keys())
 
-            obstacles.pop(idx, None)
+    for i in range(len(moves)):
+        move = moves[i]
+
+        if move[:2] in crosses and not crosses[move[:2]][0]:
+            obstacles[move[:2]] = True
+
+            crosses[move[:2]][0] = True
+            count += not chk_good_path(obstacles, list(crosses[move[:2]][1][0]), height, width)
+
+            obstacles.pop(move[:2], None)
+        
+        elif move[:2] not in crosses and move != (start[0], start[1], -1, -1):
+            obstacles[move[:2]] = True
+            
+            count += not chk_good_path(obstacles, list(moves[i - 1]), height, width)
+
+            obstacles.pop(move[:2], None)
 
     return count
 
