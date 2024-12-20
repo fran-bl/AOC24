0f054e4616aa0909d4caa738d6ebfab7e8f099f7
fran-bl <fran.blazevic0@gmail.com>
2024-12-20 23:43:53 +0100

    Optimized day 20

--- day_20/Main.py
+++ day_20/Main.py
@@ -1,12 +1,33 @@
-import heapq
-import numpy as np
-
-
-def heuristic(a, b):
+def mnhtn(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-def skip(path, r):
+def next_cell(path, cell, prev):
+    i, j = cell
+    
+    for d in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+        if d in path and d != prev:
+            return d
+
+    return None
+
+
+def construct_path(path, s, e):
+    l = 0
+    prev = None
+    c = s
+
+    while c != e:
+        path[c] = l
+        t = c
+        c = next_cell(path, c, prev)
+        prev = t
+        l += 1
+
+    path[c] = l
+
+
+def skip(path, r, save):
     count = 0
 
     for cell in path:
@@ -14,62 +35,18 @@ def skip(path, r):
             for dj in range(-r + abs(di), r + 1 - abs(di)):
                 hop = (cell[0] + di, cell[1] + dj)
                 if hop in path:
-                    if path[cell] - path[hop] - heuristic(cell, hop) >= 100:
+                    if path[cell] - path[hop] - mnhtn(cell, hop) >= save:
                         count += 1
 
     return count
 
 
-def a_star(cells, s, e):
-    g = {c: float('inf') for c in cells.keys()}
-    f = {c: float('inf') for c in cells.keys()}
-    h = {c: heuristic(c, e) for c in cells.keys()}
-    f[s], g[s] = h[s], 0
-
-    op = []
-    heapq.heappush(op, (f[s], s))
-    path, visited = {}, set()
-    found = False
-
-    while len(op) != 0:
-        curr = heapq.heappop(op)[1]
-
-        if curr == e: found = True; break
-
-        if curr in visited: continue
-
-        visited.add(curr)
-
-        for d in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            child = (curr[0] + d[0], curr[1] + d[1])
-
-            if child in cells:
-                tg = g[curr] + 1
-                tf = tg + h[child]
-
-                if tf < f[child]:
-                    g[child] = tg
-                    f[child] = tf
-                    heapq.heappush(op, (tf, child))
-                    path[child] = curr
-
-    fwd = {}
-    
-    if found:
-        c = e
-        while c != s:
-            fwd[path[c]] = c
-            c = path[c]
-
-    return len(fwd), path
+def part_1(path):
+    return skip(path, 2, 100)
 
 
-def part_1(path, r):
-    return skip(path, r)
-
-
-def part_2(path, r):
-    return skip(path, r)
+def part_2(path):
+    return skip(path, 20, 100)
 
 
 def main():
@@ -91,16 +68,10 @@ def main():
                 if data[i][j] == 'E':
                     e = (i, j)
 
-        l, p = a_star(path, s, e)
+        construct_path(path, s, e)
 
-        c = e
-        while c != s:
-            path[c] = l
-            l -= 1
-            c = p[c]
-
-        print('Part 1:', part_1(path, 2))
-        print('Part 2:', part_2(path, 20))
+        print('Part 1:', part_1(path))
+        print('Part 2:', part_2(path))
 
 
 if __name__ == '__main__':
